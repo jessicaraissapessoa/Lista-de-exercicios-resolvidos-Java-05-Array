f393e4f79b30b1348685e8fcc63e01ade54b4c6b
Jéssica Raissa Pessoa <jessicaraissapessoa@gmail.com>
2022-06-02 18:42:01 -0300

    Add files via upload

--- listajava05_visaogeral/listajava05_visaogeral.java
+++ listajava05_visaogeral/listajava05_visaogeral.java
@@ -126,27 +126,21 @@ Scanner teclado = new Scanner (System.in);
 		
 		
 		
-	/* 03. Escreva um programa que receba um vetor de 5 elementos e, em seguida,
-	multiplique os elementos pares por 3 e adicionando 1 aos elementos ímpares.
-	Mostre o vetor alterado.
-		
-	int [] vetor = new int [5];
+	/* 03. Construa um programa que leia 5 valores double e os armazene em um array 'notas', exibindo-os em seguida.
+
+	double [] notas = new double [5];
 	
 	
-	for (int indice = 0; indice < vetor.length; indice++)
+	for (int indice = 0; indice < notas.length; indice++)
 	{
-		
-		System.out.println("Informe o elemento de índice " + indice + " do vetor: ");
-		vetor[indice] = teclado.nextInt();
-		
+		System.out.println("Informe a " + (indice + 1) + "ª nota: ");
+		notas[indice] = teclado.nextDouble();
 	}
 	
-	for (int indice = 0; indice < vetor.length; indice++)
+	
+	for (double listanotas: notas)
 	{
-		
-		if (vetor[indice] %2 == 0) System.out.println(vetor[indice] * 3);
-		else System.out.println(vetor[indice] + 1);		
-		
+		System.out.println(listanotas);
 	}
 		
 		
